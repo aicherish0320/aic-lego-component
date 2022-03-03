bb94762d0411956dc2e2e5c3a9be0eb72999ed58
aicherish0320 <aicherish0320@163.com>
2022-03-04 07:49:29 +0800

    feat: add test & 发布前代码检查

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,9 @@ module.exports = {
   preset: '@vue/cli-plugin-unit-jest/presets/typescript',
   transform: {
     '^.+\\.vue$': 'vue-jest'
-  }
+  },
+  transformIgnorePatterns: [
+    '/!node_modules\\/lodash-es/',
+    '<rootDir>/node_modules/(?!lodash-es)'
+  ]
 }
