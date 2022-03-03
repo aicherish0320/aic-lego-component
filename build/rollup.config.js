2809c293211d0bb5531ba62962dbb7436e11b2ac
aicherish0320 <aicherish0320@163.com>
2022-03-03 22:21:01 +0800

    feat: 尝试打包入口文件

--- build/rollup.config.js
+++ build/rollup.config.js
@@ -1,6 +1,7 @@
 import vue from 'rollup-plugin-vue'
 import css from 'rollup-plugin-css-only'
 import typescript from 'rollup-plugin-typescript2'
+import { nodeResolve } from '@rollup/plugin-node-resolve'
 import { name } from '../package.json'
 const file = (type) => `dist/${name}.${type}.js`
 
@@ -18,10 +19,12 @@ export default {
     format: 'es'
   },
   plugins: [
+    nodeResolve(),
     typescript({
       tsconfigOverride: overrides
     }),
     vue(),
     css({ output: 'bundle.css' })
-  ]
+  ],
+  external: ['vue', 'lodash-es']
 }
